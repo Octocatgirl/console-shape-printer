89502b500203d0db334030c4239daec831ba2f4a
Shaylee <octocatgirl@gmail.com>
2022-02-25 17:22:48 -0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,56 @@
+package com.company;
+
+public class Main {
+    static void printRightTriangle(int height){
+        for(int i=0; i<height; i++){
+            printSpaces(1);
+            printLine(i);
+        }
+    }
+
+    static void printIsocelesTriangle(int height){
+        for(int i=0; i<height; i++){
+            printSpaces(height-i);
+            printLine(i + (i-1));
+        }
+    }
+
+    static void printCircle(int radius){
+//        int diameter = radius*2;
+        for(int i=0; i<radius; i++){
+            System.out.print(i+":");
+            printSpaces(radius-i);
+            printLine(radius+(i*2));
+        }
+    }
+
+    static int powerOf(int num, int power){
+        int product = num;
+        for(int i=0; i<power; i++){
+            product = product*num;
+        }
+        return product;
+    }
+
+    static void printSpaces(int spaces){
+        for(int i=0; i<spaces; i++){
+            if(i==0){
+                System.out.println("-");
+            }
+            System.out.print("-");
+        }
+    }
+
+    static void printLine(int length){
+        for(int i=0; i<length; i++){
+            System.out.print("@");
+        }
+    }
+
+    public static void main(String[] args) {
+//      printRightTriangle(9);
+//      printIsocelesTriangle(9);
+      printCircle(9);
+    }
+}
+
